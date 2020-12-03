4f209a154243d8be8b7bdd4dd33404cd0845b5c9
zhongyuanjia <w278206624@gmail.com>
2020-12-03 16:56:31 +0800

    feat(event-key): support prefix

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,25 @@ import { EventEmitter } from "events";
 
 export type Listener = (...args: any) => void | Promise<void>;
 
+export type AsyncEventOptions = {
+  prefix?: string;
+};
+
 export class AsyncEventEmitter {
   protected eventEmitter = new EventEmitter();
 
-  off(event: string | symbol, listener?: Listener) {
+  constructor(protected readonly options?: AsyncEventOptions) {}
+
+  protected getKey(event: string) {
+    const p = this.options?.prefix;
+    if (p) {
+      return p + event;
+    }
+    return event;
+  }
+
+  off(event: string, listener?: Listener) {
+    event = this.getKey(event);
     if (listener) {
       this.eventEmitter.off(event, listener);
     } else {
@@ -14,12 +29,14 @@ export class AsyncEventEmitter {
     return this;
   }
 
-  on(event: string | symbol, listener: Listener) {
+  on(event: string, listener: Listener) {
+    event = this.getKey(event);
     this.eventEmitter.on(event, listener);
     return this;
   }
 
-  async emit(event: string | symbol, ...args: any) {
+  async emit(event: string, ...args: any) {
+    event = this.getKey(event);
     const listeners = this.eventEmitter.listeners(event);
     if (!listeners.length) {
       return false;
